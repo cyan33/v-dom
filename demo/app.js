12e7a2930db8513bca807638eaa5802a3e415c19
cyan33 <thomasyim94@gmail.com>
2018-03-15 21:26:37 -0400

    initial commit

---
+++ demo/app.js
@@ -0,0 +1,25 @@
+const { render, hyperscript, setState } = require('../src')
+
+function createComponent(count) {
+  const r = [...Array(count).keys()]
+  return (
+    <ul id="cool" className={`my-class-${count % 3}`}>
+      { r.map(n => <li>{(count).toString()}</li>) }
+    </ul>
+  )
+}
+
+function inc(parent, count) {
+  if (count > 20) return
+  console.log(count)
+  setState(parent, createComponent(count), createComponent(count + 1))
+
+  setTimeout(() => inc(parent, count + 1), 500)
+}
+
+(function() {
+  const root = document.getElementById('root')
+  render(createComponent(1), root)
+
+  setTimeout(() => inc(root, 1), 500)
+})()
